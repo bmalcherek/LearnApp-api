df9a66aadb40e4d459bee9bcd5794967ce8f4baf
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-08-01 12:03:20 +0200

    heroku config

--- learn_app/settings.py
+++ learn_app/settings.py
@@ -131,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
